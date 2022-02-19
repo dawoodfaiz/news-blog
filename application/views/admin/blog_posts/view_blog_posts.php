bea6c6b72c3747b0fc0dc5e8a51db643f09affb9
dawoodfaiz <dawoodfaiz.2@gmail.com>
2022-02-19 13:11:16 +0500

    Update view_blog_posts.php

--- application/views/admin/blog_posts/view_blog_posts.php
+++ application/views/admin/blog_posts/view_blog_posts.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('admin/dashboard/header'); ?>
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-      <h2>View Blog Posts </h2>
+      <h2 class="blog-title">All Blog Posts </h2>
 
     </main>
   </div>
